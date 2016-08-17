d24ee82c393b7ee58c45d722ec9291c3113fe435
Patrick Cloutier <pcloutier@connexe.ca>
2016-08-17 12:39:48 -0400

    - Added instructions for registering the custom ELFinder model binder with ASP.NET MVC

--- ASP.NET/ELFinder.WebServer.ASPNet/Controllers/ELFinderController.cs
+++ ASP.NET/ELFinder.WebServer.ASPNet/Controllers/ELFinderController.cs
@@ -5,6 +5,8 @@ namespace ELFinder.WebServer.ASPNet.Controllers
 
     /// <summary>
     /// ELFinder controller
+    /// IMPORTANT: If you use this controller in your existing application, make sure to register the custom ELFinder model binder (ELFinderModelBinder) with ASP.NET MVC.
+    ///            Look at the Application_Start() method in Global.asax.cs on how to do so.
     /// </summary>
     public class ELFinderController : Controller
     {
